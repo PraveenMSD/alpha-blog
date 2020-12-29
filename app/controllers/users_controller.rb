d0a9a5bb281d1f33491118bdb814569ecab84fd2
PraveenMSD <praveenrk189@gmail.com>
2020-12-29 09:04:51 +0530

    Created user create action and added sign up link to home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "Welcome to Praveen Blog, you have successfully signed up"
+            flash[:notice] = "Welcome to Praveen Blog #{@user.username}, you have successfully signed up"
             redirect_to articles_path
         else
             render 'new'
